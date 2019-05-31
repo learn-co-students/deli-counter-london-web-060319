84ff4f94ef4d402da211058f433a9a8d1277128b
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 11:23:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,8 @@ katz_deli = []
 def line(katz_deli)
   number = 1
   deli_line = []
-
   if katz_deli.length == 0
     puts "The line is currently empty."
-
   else
       katz_deli.each do |customer_name|
       deli_line.push("#{number}. #{customer_name}")
